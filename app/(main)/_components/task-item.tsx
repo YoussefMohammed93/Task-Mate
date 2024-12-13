cd4df007234fa545201d628a830b2bb936bd4e4d
YoussefMohammed93 <youssefmohammed1590@gmail.com>
2024-12-13 14:31:53 +0200

    Sidebar done

--- app/(main)/_components/task-item.tsx
+++ app/(main)/_components/task-item.tsx
@@ -42,14 +42,14 @@ const TaskItem = ({
     <div
       role="button"
       onClick={handleClick}
-      className={`group w-full min-h-[28px] flex items-center text-sm font-medium py-1 px-5 hover:bg-primary/5 cursor-pointer ${
+      className={`group w-full min-h-[28px] flex items-center text-sm font-medium py-1.5 px-5 hover:bg-primary/5 cursor-pointer ${
         isActive ? "bg-primary/5 text-primary" : "text-muted-foreground"
       } ${!isSearch ? "border-y" : ""} 
       ${isFirst ? "border-t-2" : ""} 
       ${isLast ? "border-b-2" : ""}`}
     >
-      <Icon className="h-[18px] w-[18px] shrink-0 mr-2" />
-      <span className="truncate mt-0.5">{label}</span>
+      <Icon className="size-5 shrink-0 mr-2" />
+      <span className="truncate">{label}</span>
       {isSearch && (
         <kbd className="hidden md:inline-flex items-center gap-1 rounded h-6 text-[10px] ml-auto pointer-events-none select-none px-1.5 border bg-muted font-mono font-medium text-muted-foreground opacity-100">
           <span className="text-xs">CTRL + K</span>
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,10 +4,13 @@ import {
   CalendarDays,
   ChevronsRight,
   ListChecks,
+  LogOut,
   NotepadText,
   Search,
+  Settings,
   XIcon,
 } from "lucide-react";
+import { useClerk } from "@clerk/clerk-react";
 import { useSearch } from "@/hooks/use-search";
 import { useSidebar } from "./contexts/sidebar-context";
 import TaskItem from "@/app/(main)/_components/task-item";
@@ -15,21 +18,22 @@ import TaskItem from "@/app/(main)/_components/task-item";
 export const Sidebar = () => {
   const search = useSearch();
   const { isSidebarVisible, toggleSidebar } = useSidebar();
+  const { signOut } = useClerk();
 
   return (
     <div
       className={`fixed h-screen transition-all z-50 duration-300 overflow-hidden bg-gray-100 ${
         isSidebarVisible ? "w-0" : "w-full absolute"
-      } md:w-[250px] md:block`}
+      } md:w-[250px] flex flex-col`}
     >
       <div className="w-full absolute top-[6.9vh] h-[93.1vh] -z-10 border-r"></div>
       <button className="md:hidden py-2.5 px-5" onClick={toggleSidebar}>
         <XIcon />
       </button>
-      <div className="hidden md:block text-xl font-semibold absolute top-2.5 left-5">
+      <div className="block text-xl font-semibold absolute top-2.5 right-5 md:left-5">
         Taskmate
       </div>
-      <div className="pt-2 md:pt-12">
+      <div className="flex-grow pt-2 md:pt-12">
         <TaskItem
           onClick={search.onOpen}
           label="Search"
@@ -37,34 +41,52 @@ export const Sidebar = () => {
           isSearch
           toggleSidebar={toggleSidebar}
         />
+        <div className="pl-5 pt-5 pb-2 font-medium">Tasks</div>
+        <div>
+          <TaskItem
+            label="Today"
+            path="/"
+            icon={ListChecks}
+            isFirst
+            toggleSidebar={toggleSidebar}
+          />
+          <TaskItem
+            label="Upcoming"
+            path="/upcoming"
+            icon={ChevronsRight}
+            toggleSidebar={toggleSidebar}
+          />
+          <TaskItem
+            label="Calendar"
+            path="/calendar"
+            icon={CalendarDays}
+            toggleSidebar={toggleSidebar}
+          />
+          <TaskItem
+            label="Sticky Wall"
+            path="/sticky-wall"
+            icon={NotepadText}
+            isLast
+            toggleSidebar={toggleSidebar}
+          />
+        </div>
       </div>
-      <div className="pl-5 pt-5 pb-2 font-medium">Tasks</div>
-      <TaskItem
-        label="Today"
-        path="/"
-        icon={ListChecks}
-        isFirst
-        toggleSidebar={toggleSidebar}
-      />
-      <TaskItem
-        label="Upcoming"
-        path="/upcoming"
-        icon={ChevronsRight}
-        toggleSidebar={toggleSidebar}
-      />
-      <TaskItem
-        label="Calendar"
-        path="/calendar"
-        icon={CalendarDays}
-        toggleSidebar={toggleSidebar}
-      />
-      <TaskItem
-        label="Sticky Wall"
-        path="/sticky-wall"
-        icon={NotepadText}
-        isLast
-        toggleSidebar={toggleSidebar}
-      />
+      <div>
+        <TaskItem
+          label="Settings"
+          path="/settings"
+          icon={Settings}
+          isFirst
+          toggleSidebar={toggleSidebar}
+        />
+        <TaskItem
+          label="Log out"
+          onClick={signOut}
+          icon={LogOut}
+          isLast
+          toggleSidebar={toggleSidebar}
+        />
+      </div>
     </div>
   );
 };
